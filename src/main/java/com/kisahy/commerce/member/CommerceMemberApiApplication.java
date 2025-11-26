733a77fb851fdc306944bc9d59711a0c4cff524d
Gaffel <gaffel@ip-192-168-10-17.ap-northeast-2.compute.internal>
2025-11-26 17:31:34 +0900

    init project

---
+++ src/main/java/com/kisahy/commerce/member/CommerceMemberApiApplication.java
@@ -0,0 +1,13 @@
+package com.kisahy.commerce.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceMemberApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceMemberApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kisahy/commerce/member/CommerceMemberApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kisahy.commerce.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceMemberApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
